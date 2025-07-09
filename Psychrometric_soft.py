041b084e91e6055012252832199b8edeb53aee57
Eduardo Andres Delgadillo Monsalve <eduardo100796@gmail.com>
2025-07-09 10:27:35 -0500

    patente docuemento

--- Psychrometric_soft.py
+++ Psychrometric_soft.py
@@ -19,7 +19,7 @@ def calculate():
     try:
         Tdb = float(entry_temp.get())  # Dry bulb temperature (°C)
         RH = float(entry_rh.get()) / 100.0  # Relative Humidity (0-1)
-        P_atm = 102800  # Atmospheric pressure (Pa)
+        P_atm = 101325  # Atmospheric pressure (Pa)
 
         # Psychrometric calculations
         Psat = GetSatVapPres(Tdb)  # Saturation vapor pressure at Tdb
@@ -38,17 +38,17 @@ def calculate():
         Tsat = Twb
 
         result_text.set(
-            f"Atmospheric Pressure: {P_atm:.2f} Pa\n"
-            f"Dry Bulb Temperature: {Tdb:.2f} °C\n"
-            f"Relative Humidity: {RH*100:.2f} %\n"
-            f"Humidity Ratio: {W_g_per_kg:.3f} g/kg dry air\n"
-            f"Dew Point Temperature: {Tdp:.2f} °C\n"
-            f"Wet Bulb Temperature: {Twb:.2f} °C\n"
-            f"Saturation Temperature: {Tsat:.2f} °C (approx)\n"
-            f"Enthalpy: {h:.2f} kJ/kg dry air\n"
-            f"Vapor Pressure: {Pv:.2f} Pa\n"
-            f"Saturation Vapor Pressure: {Psat:.2f} Pa\n"
-            f"Specific Volume: {v:.3f} m³/kg dry air\n"
+            #f"Atmospheric Pressure: {P_atm:.2f} Pa\n"
+            #f"Dry Bulb Temperature: {Tdb:.2f} °C\n"
+            #f"Relative Humidity: {RH*100:.2f} %\n"
+            #f"Humidity Ratio: {W_g_per_kg:.3f} g/kg dry air\n"
+            #f"Dew Point Temperature: {Tdp:.2f} °C\n"
+            #f"Wet Bulb Temperature: {Twb:.2f} °C\n"
+            #f"Saturation Temperature: {Tsat:.2f} °C (approx)\n"
+            #f"Enthalpy: {h:.2f} kJ/kg dry air\n"
+            #f"Vapor Pressure: {Pv:.2f} Pa\n"
+            #f"Saturation Vapor Pressure: {Psat:.2f} Pa\n"
+            #f"Specific Volume: {v:.3f} m³/kg dry air\n"
             f"Density: {rho:.3f} kg/m³"
         )
 
@@ -324,7 +324,18 @@ def plot_psychrometric_chart(Tdb, RH, P_atm, W, Tdp, Twb, Pv, Psat, enthalpy):
     ax_dew.set_ylim(ax.get_ylim())
 
     # Crear ticks equidistantes
-    tick_positions = np.linspace(0,70,8)  # 8 ticks entre 0 y 70
+    #tick_positions = np.linspace(0,70,8)  # 8 ticks entre 0 y 70
+    tick_positions = [
+        5.397,
+        7.618,
+        10.626,
+        14.662,
+        20.034,
+        27.142,
+        36.508,
+        48.824,
+        65.027,
+    ]
     dew_labels = []
     Tdb_fixed = 180  # °C fijo para que el cálculo de RH sea válido
 
